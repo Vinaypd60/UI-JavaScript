5094e4c50c36052a61e40f9d27c2e3040b3e4608
Vinay <96037654+Vinaypd60@users.noreply.github.com>
2024-05-16 09:19:32 +0530

    Add files via upload

---
+++ Hoisting - Copy.js
@@ -0,0 +1,42 @@
+var city;
+
+console.log(city);
+//it will print undefined value it is a spcl value in JS.
+//This is assigned to variable
+//if we declare a variable and dont assign any value to it.
+//undefined data type is also undefined.
+
+var city = "delhi";
+
+console.log("city"); 
+//it will print here delhu bcz we assign a var.
+
+console.log(city); //undefined
+var city; // this process i called shadowing
+//1st we are using the variable and after that we are assigning the value.it will print undefined.
+
+console.log(username); //undefined
+var username = "XYZ";
+console.log(username); //XYZ
+
+
+console.log(city);
+let city; // we cannot access the city before Initialization
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
