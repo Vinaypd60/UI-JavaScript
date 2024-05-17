1ed0b01fb16b46023353366d0ddca337c86a843d
Vinay <96037654+Vinaypd60@users.noreply.github.com>
2024-05-17 08:58:27 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+<html>
+    <head>
+    <style></style>
+    
+    <script src="index.html"></script>
+        <script>
+            var city = "Hyderabad"
+            function f1(){
+            }
+        </script>
+    </head>
+    <body>
+
+    </body>
+</html>
